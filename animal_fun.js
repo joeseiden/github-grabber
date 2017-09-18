10814e5388b9b12700f8974305fef23c6f0d6753
Joe Seiden <joe.seiden214@gmail.com>
2017-09-18 16:43:05 -0700

    Do writeFile exercise

--- animal_fun.js
+++ animal_fun.js
@@ -1,9 +1,18 @@
 const fs = require('fs');
 
-fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(data);
+// })
+//
+
+fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
   if (err) {
     console.log(err);
-    return;
+  } else {
+    console.log("file successfully written!");
   }
-  console.log(data);
 })
