7885b576a0303351582753d7e850af532dc65d87
Joe Seiden <joe.seiden214@gmail.com>
2017-09-18 16:45:44 -0700

    Learn how to process.argv

--- animal_fun.js
+++ animal_fun.js
@@ -9,10 +9,12 @@ const fs = require('fs');
 // })
 //
 
-fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("file successfully written!");
-  }
-})
+// fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("file successfully written!");
+//   }
+// })
+
+console.log(process.argv);
