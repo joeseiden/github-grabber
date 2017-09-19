83101293d57ef8cfbff745c6ab0e9c32f2d7510c
Joe Seiden <joe.seiden214@gmail.com>
2017-09-18 17:07:11 -0700

    Finish part 1

--- animal_fun.js
+++ animal_fun.js
@@ -1,20 +1,21 @@
 const fs = require('fs');
+let letter = process.argv[2].toLowerCase();
 
-// fs.readFile('./animals.txt', 'utf-8', (err, data) => {
-//   if (err) {
-//     console.log(err);
-//     return;
-//   }
-//   console.log(data);
-// })
-//
-
-// fs.writeFile('./example.txt', 'I will be written to example.txt', err => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     console.log("file successfully written!");
-//   }
-// })
-
-console.log(process.argv);
+fs.readFile('./animals.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    let selectedAnimals = data.split("\n").filter((animal) => {
+      if (typeof animal === "string" && animal[0]) {
+        return animal[0].toLowerCase() === letter;
+      }
+    });
+    fs.writeFile(`${letter}_animals.txt`, selectedAnimals.join("\n"), err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`file successfully written to ${letter}_animals.txt!`);
+      }
+    })
+  }
+})
